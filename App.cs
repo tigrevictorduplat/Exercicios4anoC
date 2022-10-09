c14a26e5a93bafb478479fdc125bf01a24db1626
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2022-10-08 22:46:33 -0300

    Update App.cs

--- App.cs
+++ App.cs
@@ -4,22 +4,15 @@
     {
         static public void Main(String[] args)
         {
-            /*
-            Console.WriteLine("Questão 1");
-            CalculadoraLP.calculaMediaIdade();
-
-            Console.WriteLine("Questão 2");
-            CalculadoraLP.calculaBaskara();
-
-            Console.WriteLine("Questão 3");
-            CalculadoraLP.calculaIMC();
-            Console.ReadKey();
-            */
-
-            //CalculadoraLoop.somarEnquanto();
-            //CalculadoraLoop.calculaFatorial();
-            //CalculadoraLoop.imprimeIntervalo();
-            //CalculadoraLoop.tabelaConversaoFahrenheitCelsius();
+            //Questão - Sala
+            CalculadoraLoop.somarEnquanto();
+            //Questão 1 - Atv 2
+            CalculadoraLoop.calculaFatorial();
+            //Questão 2 - Atv 2
+            CalculadoraLoop.imprimeIntervalo();
+            //Questão 3 - Atv 2
+            CalculadoraLoop.tabelaConversaoFahrenheitCelsius();
+            //Questão 4 - Atv 2
             CalculadoraLoop.sistemaEleitoral();
         }
     }
